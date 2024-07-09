cbbcdfb4bc44cee119b543b7c400d1c1e094fa8b
DariaSavchenko12 <pogorelskykhdaria@gmail.com>
2024-07-08 23:03:03 -0400

    create SymundCloud project

---
+++ src/test/java/com/SymundCloud/pages/LoginPage.java
@@ -0,0 +1,33 @@
+package com.SymundCloud.pages;
+
+
+import com.SymundCloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "user")
+    public WebElement userName;
+
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(name = "submit-form")
+    public WebElement submit;
+
+
+    public void login(String userNameStr, String passwordStr) {
+        userName.sendKeys(userNameStr);
+        password.sendKeys(passwordStr);
+        submit.click();
+        // verification that we logged
+    }
+
+}
