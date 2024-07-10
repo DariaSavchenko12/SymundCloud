4475bc31804ef3a974504e4a13b88117084a4021
DariaSavchenko12 <pogorelskykhdaria@gmail.com>
2024-07-10 15:10:49 -0400

    updated LoginStepDefs and Config properties

--- src/test/java/com/SymundCloud/step_definitions/LoginStepDefs.java
+++ src/test/java/com/SymundCloud/step_definitions/LoginStepDefs.java
@@ -3,42 +3,37 @@ package com.SymundCloud.step_definitions;
 import com.SymundCloud.pages.LoginPage;
 import com.SymundCloud.utilities.ConfigurationReader;
 import io.cucumber.java.en.Given;
+
 public class LoginStepDefs {
 
 
     @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() {
-        System.out.println("Login to app in Before method");
+         System.out.println("Login to app in Before method");
     }
 
     @Given("the user logged in as {string}")
     public void the_user_logged_in_as(String userType) {
         //based on input enter that user information
-        String username =null;
-        String password =null;
+        String username = null;
+        String password = null;
 
-        if(userType.equalsIgnoreCase("users")){
+        if (userType.equalsIgnoreCase("users")) {
             username = ConfigurationReader.getProperty("users_username");
             password = ConfigurationReader.getProperty("users_password");
-        }else if(userType.equalsIgnoreCase("employee")){
+        } else if (userType.equalsIgnoreCase("employee")) {
             username = ConfigurationReader.getProperty("employee_username");
             password = ConfigurationReader.getProperty("employee_password");
-        }else if(userType.equalsIgnoreCase("store manager")){
-            username = ConfigurationReader.getProperty("store_manager_username");
-            password = ConfigurationReader.getProperty("store_manager_password");
         }
         //send username and password and login
-        new LoginPage().login(username,password);
+        new LoginPage().login(username, password);
     }
 
     @Given("the user logged in with username as {string} and password as {string}")
     public void the_user_logged_in_with_username_as_and_password_as(String username, String password) {
-      LoginPage loginPage=new LoginPage();
-      loginPage.login(username,password);
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(username, password);
     }
-
-
-
 
 
 }
