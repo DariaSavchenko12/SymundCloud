06932fb539246e658b1a8daba74570ea5ad110d7
robertajovaisa <roberta.jovaisa@gmail.com>
2024-07-15 23:15:06 -0500

    last touch ups

--- src/test/java/com/SymundCloud/pages/FilesPage.java
+++ src/test/java/com/SymundCloud/pages/FilesPage.java
@@ -1,12 +1,18 @@
 package com.SymundCloud.pages;
 
+import com.SymundCloud.utilities.BrowserUtils;
 import com.SymundCloud.utilities.Driver;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+import java.util.Iterator;
 import java.util.List;
 
 public class FilesPage {
@@ -26,29 +32,58 @@ public class FilesPage {
 
         List<WebElement> allUploadedFiles = Driver.getDriver().findElements(By.cssSelector("span.innernametext"));
         boolean result = false;
-        for (WebElement eachUploadedFile : allUploadedFiles) {
-            if (eachUploadedFile.getText().equals(fileName));
-            result=true;
-        }
-        Assert.assertTrue(result);
 
+        for (WebElement eachUploadedFile : allUploadedFiles) {
+             if (eachUploadedFile.getText().equals(fileName)){
+                 result=true;
+             break; // exits the loop once matching file is found
+             }
+        }
+     //   Driver.getDriver().navigate().refresh();
+        Assert.assertTrue("File not found: " + fileName,result);
     }
 
+
+    @FindBy(css = "#file_upload_start")
+    public WebElement hiddenUploadFile;
+
+    @FindBy(css = "#uploadprogressbar")
+    public WebElement progressbar;
+
+    public void waitUntilProgressbarDisappear() {
+        try {
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.invisibilityOf(progressbar));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    // UPLOAD FILE METHOD
+    public void uploadFile (String fileName){
+        addModule.click();
+        //uploadFile.sendKeys(System.getProperty("user.dir") + "/src/test/resources/uploadedFiles/"+fileName);
+        BrowserUtils.waitFor(1);
+       // uploadFileOption.click();
+        Actions actions = new Actions(Driver.getDriver());
+        actions.sendKeys(hiddenUploadFile,System.getProperty("user.dir") + "/src/test/resources/uploadedFiles/"+fileName);
+        hiddenUploadFile.sendKeys((System.getProperty("user.dir") + "/src/test/resources/uploadedFiles/"+fileName));
+        waitUntilProgressbarDisappear();
+        BrowserUtils.waitFor(3);
+    }
 
 
 
     @FindBy (css = "a.button.new")
     public WebElement addModule;
 
-    @FindBy (xpath = "//span[.='Upload file']")
-    public WebElement uploadFileOption;
-
     @FindBy (id = "view12-input-folder")
     public WebElement folderNameInput;
 
-    @FindBy (xpath = "(//input[@type='submit'])[1]")
-    public WebElement folderInputArrow;
-
+    @FindBy (xpath = "(//span[@class='innernametext'])[1]")
+    public WebElement firstFileOnTheList;
 
     @FindBy (xpath = "(//a[@data-action='menu'])[1]")
     public WebElement threeDotMenu;
@@ -59,14 +94,28 @@ public class FilesPage {
 
 
 
-    @FindBy (xpath = "//span[@class='info']")
-    public WebElement allFilesSummary;
+    // old method, will delete soon
+    /*
+    public void deletingFiles0 (String fileName){
+       List<WebElement> allThreeDots = Driver.getDriver().findElements(By.xpath("//span[.='" + fileName + "']/../span/a[2]"));
+
+        for (WebElement eachThreeDot : allThreeDots) {
+            if (eachThreeDot.toString().contains(fileName)){
+                eachThreeDot.click();
+            }
+        }
+        deleteFolderOption.click();
+
+    }
+     */
+
+    // DYNAMIC METHOD TO DELETE FILES BASED ON THEIR NAME
+    public void deletingFiles (String fileName){
+        WebElement threeDotMenu = Driver.getDriver().findElement(By.xpath("//span[.='" + fileName + "']/../span/a[2]"));
+        threeDotMenu.click();
+        deleteFolderOption.click();
+    }
 
 
-    @FindBy (xpath = "//span[@class='dirinfo']")
-    public WebElement folderSummary;
-
-    @FindBy (xpath = "//span[@class='fileinfo']")
-    public WebElement filesSummary;
 
 }
--- src/test/java/com/SymundCloud/step_definitions/FilesStepDefs.java
+++ src/test/java/com/SymundCloud/step_definitions/FilesStepDefs.java
@@ -6,9 +6,14 @@ import com.SymundCloud.utilities.BrowserUtils;
 import com.SymundCloud.utilities.Driver;
 import io.cucumber.java.en.*;
 import org.junit.Assert;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 public class FilesStepDefs extends BasePage {
@@ -28,28 +33,38 @@ public class FilesStepDefs extends BasePage {
         Assert.assertTrue(actualTitle.contains(argFiles));
     }
 
+
+    @Then("user uploads a file")
+    public void user_uploads_a_file() {
+        filesPage.uploadFile("uploadFile.txt");
+        BrowserUtils.sleep(1);
+    }
+
+
+    @Then("user can see uploaded file in the list")
+    public void user_can_see_uploaded_file_in_the_list() {
+        filesPage.verifyFileExistence("uploadFile");
+    }
+
+
+    @Then("user clicks {string} file delete option")
+    public void userClicksFileDeleteOption(String arg0) {
+        filesPage.deletingFiles(arg0);
+    }
+
+
+    // 2 SCENARIO
+
     @When("user clicks plus sign module")
     public void user_clicks_plus_sign_module() {
         filesPage.addModule.click();
     }
 
+
     @Then("user sees and clicks {string} option")
     public void user_sees_and_clicks_option(String argUploadFile) {
         filesPage.selectAddOption(argUploadFile);
     }
-
-
-    @Then("user uploads a file")
-    public void user_uploads_a_file() {
-        filesPage.uploadFileOption.sendKeys("/Users/robertajovaisa/Desktop/UI Test Automation/Project Automation Sprint/uploadFile" + Keys.ENTER);
-        BrowserUtils.sleep(10);
-    }
-    @Then("user can see uploaded file in the list")
-    public void user_can_see_uploaded_file_in_the_list() {
-
-    }
-
-    // 2 SCENARIO
 
     @Then("user enters New folder name")
     public void userEntersNewFolderName() {
@@ -63,6 +78,9 @@ public class FilesStepDefs extends BasePage {
 
     @And("user can see new folder in the list")
     public void userCanSeeNewFolderInTheList() {
+        WebElement createdFolder = Driver.getDriver().findElement(By.xpath("(//span[.='B34'])[2]"));
+        BrowserUtils.waitForVisibility(createdFolder, 5);
+
         filesPage.verifyFileExistence("B34");
     }
 
@@ -70,19 +88,31 @@ public class FilesStepDefs extends BasePage {
 
     // 3 SCENARIO
 
-    @When("user clicks three dots menu next to the file")
-    public void userClicksThreeDotsMenuNextToTheFile() {
+    @When("user clicks three dots menu next to the first file")
+    public void userClicksThreeDotsMenuNextToTheFirstFile() {
+        filesPage.threeDotMenu.click();
     }
 
-    @Then("user sees and clicks delete option")
-    public void userSeesAndClicksDeleteOption() {
+    @Then("user deletes that file and verifies it no longer exist")
+    public void userDeletesThatFileAndVerifiesItNoLongerExist() {
+        String nameOfFirstFileOnTheList = filesPage.firstFileOnTheList.getText(); // in this line I am saving name of the line I am deleting
+        filesPage.deleteFolderOption.click();
+
+        BrowserUtils.waitForInvisibilityOf(Driver.getDriver().findElement(By.xpath("(//span[.='" + nameOfFirstFileOnTheList + "'])[3]")));
+        Assert.assertNotEquals(nameOfFirstFileOnTheList,filesPage.firstFileOnTheList.getText());   // verifying new files new is different
     }
 
-    @And("user sees file is no longer on the list")
-    public void userSeesFileIsNoLongerOnTheList() {
-    }
+
+
+    // 4 SCENARIO
 
     @And("user should see total number of files and folders displayed under the files list")
     public void userShouldSeeTotalNumberOfFilesAndFoldersDisplayedUnderTheFilesList() {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        WebElement allFilesSummary = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='info']")));
+        Assert.assertTrue(allFilesSummary + " : ",allFilesSummary.isDisplayed());
     }
+
+
+
 }
